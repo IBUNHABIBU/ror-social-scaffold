32bbadf591f7e7e8cdf1d73133dbcd6424cb1806
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-23 12:49:35 +0000

    friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-  
+  enum status: { pending: 0, requested: 1, accepted: 2, blocked: 3 }
+  # id: 1
+  # friend: 2
+
+  # user_id: 1, friend_id: 2, status: :requested
+  # user_id: 2, friend_id: 1, status: :pending
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,35 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
-  def friend_names
-    friends.map { |e| e.name }
+  
+   has_many :friendships
+  has_many :friends, -> { where friendships: { status: :accepted }}, through: :friendships
+  has_many :requested_friends, -> { where friendships: { status: :requested }}, through: :friendships, source: :friend
+  has_many :pending_friends, -> { where friendships: { status: :pending }}, through: :friendships, source: :friend
+  has_many :blocked_friends, -> { where friendships: { status: :blocked }}, through: :friendships, source: :friend
+
+  # has_many :friendships_inverse, class_name: 'Friendship', foreign_key: :friend_id
+  # has_many :friends_inverse, through: :friendships_inverse, source: :user
+
+  # def all_friends
+  #   friends + friends_inverse
+  # end
+
+  def friend_request(friend)
+    unless self == friend || Friendship.where(user: self, friend: friend).exists?
+      transaction do
+        Friendship.create(user: self, friend: friend, status: :pending)
+        Friendship.create(user: friend, friend: self, status: :requested)
+      end
+    end
   end
-  def is_friend?(another_user)
-    friends.include?(another_user)
-  end
-  def add_friend(another_user)
-    friends << another_user 
+
+  # jane.accept_request(john)
+  # john.accept_request(jane) ! bad
+  def accept_request(friend)
+    transaction do
+      Friendship.find_by(user: self, friend: friend, status: [:requested])&.accepted!
+      Friendship.find_by(user: friend, friend: self, status: [:pending])&.accepted!
+    end
   end
 end
