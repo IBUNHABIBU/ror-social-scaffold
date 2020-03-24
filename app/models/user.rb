98f6da3b14b1573c0e79e61d5fdf50599baf24a7
maya88en <maya88en@gmail.com>
2020-03-24 12:49:26 +0200

    add friend request

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,22 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
+
   has_many :friendships
   has_many :friends, -> { where friendships: { status: :accepted }}, through: :friendships
   has_many :requested_friends, -> { where friendships: { status: :requested }}, through: :friendships, source: :friend
   has_many :pending_friends, -> { where friendships: { status: :pending }}, through: :friendships, source: :friend
   has_many :blocked_friends, -> { where friendships: { status: :blocked }}, through: :friendships, source: :friend
+
+  def friend_request(friend)
+    unless self == friend || Friendship.where(user: self, friend: friend).exists?
+      transaction do
+        Friendship.create(user: self, friend: friend, status: :pending)
+        Friendship.create(user: friend, friend: self, status: :requested)
+      end
+    end
+  end
+
 end
+
+
