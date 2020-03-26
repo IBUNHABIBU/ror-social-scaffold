175edf8454fce6059bb1fc93b91e386ff69021d1
maya88en <maya88en@gmail.com>
2020-03-26 17:19:12 +0200

    create user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    let(:user) { build(:user) }
+    context 'Creating User' do
+      it 'should give a user name' do
+        user.name = 'Maya'
+        expect(user.name).to eq('Maya')
+      end
+      it 'should give the user email' do
+        user.email = 'maya@gmail.com'
+        expect(user.email).to eq('maya@gmail.com')
+      end
+    end
+end
