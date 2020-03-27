33e9f31a5ffb7e37a0f2f1e39ffc247df15caa64
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-27 16:25:14 +0000

    testing user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe User, type: :model do
         expect(user.email).to eq('maya@gmail.com')
       end
     end
+    describe 'validations' do
+      it { should validate_presence_of(:name) }
+    end
+    it do
+        should validate_length_of(:name).is_at_most(20).on(:create)
+    end
+  context 'User Associations' do
+    it { should have_many(:friendships) }
+    it { should have_many(:requested_friends).through(:friendships).source(:friend) }
+    it { should have_many(:friends).through(:friendships) }
+    it { should have_many(:pending_friends).through(:friendships).source(:friend) }
+    it { should have_many(:blocked_friends).through(:friendships).source(:friend) }
+  end
 end
