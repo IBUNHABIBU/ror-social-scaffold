42dcc31a5485a1348477315b5d1c4e84d132b0f0
IBUNHABIBU <ibunhabib@gmail.com>
2020-03-30 14:16:28 +0000

    correcting error

--- spec/features/friend_request/friend_request_spec.rb
+++ spec/features/friend_request/friend_request_spec.rb
@@ -4,8 +4,6 @@ RSpec.feature " Following friend " do
   before do 
     @maya = User.create(name:"Maya",email:"maya@gmail.com",password:"123456")
     @juma = User.create(name:"Jumaa",email:"juma@gmail.com",password:"123456")
-    @isack = User.create(name:"Isack",email:"isack@gmail.com",password:"123456")
-    @makala = User.create(name:"Makala",email:"makala@gmail.com",password:"123456")
     login_as(@juma)
   end
   scenario 'List of all users with friendship status' do 
@@ -13,11 +11,12 @@ RSpec.feature " Following friend " do
     expect(page).to have_content(@maya.name)
     expect(page).to have_content(@juma.name)
     
-    href = "friendships?friend_id=#{@juma.id}"
+    href = "/friendships?friend_id=#{@juma.id}"
     expect(page).not_to have_link("Add Friend", :href => href)
-     link = "a[href='/friendships?friend_id=#{@maya.id}']"
-    find(link, :visible => false)
-    href = "friendships?friend_id=#{@maya.id}"
-    # expect(page).to have_link("Remove Request", :href => href)
+    
+    link = "a[href='/friendships?friend_id=#{@maya.id}']"
+    find(link)
+     href = "/friendships?friend_id=#{@maya.id}"
+     expect(page).to have_link("Add Friend", :href => href)
   end
 end
