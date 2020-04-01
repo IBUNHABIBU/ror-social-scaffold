90811dfadb1f43cad08ec0f0d53d75a27e993971
IBUNHABIBU <ibunhabib@gmail.com>
2020-04-01 10:41:52 +0000

    making correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true, length: { maximum: 20 }
-
-  has_many :posts
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-
   has_many :friendships
   has_many :friends, -> { where friendships: { status: :accepted } }, through: :friendships
   has_many :requested_friends, -> { where friendships: { status: :requested } }, through: :friendships, source: :friend
   has_many :pending_friends, -> { where friendships: { status: :pending } }, through: :friendships, source: :friend
   has_many :blocked_friends, -> { where friendships: { status: :blocked } }, through: :friendships, source: :friend
+
+  # has_many :friendships_inverse, class_name: 'Friendship', foreign_key: :friend_id
+  # has_many :friends_inverse, through: :friendships_inverse, source: :user
+
+  # def all_friends
+  #   friends + friends_inverse
+  # end
 
   def friendship?(friend)
     return true if self == friend
@@ -29,7 +27,7 @@ class User < ApplicationRecord
   end
 
   def pending_friends_with?(friend)
-    return false unless self == friend
+    return false if self == friend
 
     pending_friends.map(&:id).include?(friend.id)
   end
@@ -49,6 +47,8 @@ class User < ApplicationRecord
     end
   end
 
+  # jane.accept_request(john)
+  # john.accept_request(jane) ! bad
   def accept_request(friend)
     transaction do
       Friendship.find_by(user: self, friend: friend, status: [:requested])&.accepted!
