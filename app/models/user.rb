f425fc0ae6d4b3e6d603b9dc8d00069e5bb22ee7
IBUNHABIBU <ibunhabib@gmail.com>
2020-04-07 11:22:28 +0000

    user validations test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true, length: { maximum: 20 }
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
