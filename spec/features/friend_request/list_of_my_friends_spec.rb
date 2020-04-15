6f008f874ed89652e2e83b95851e6b98eb867765
IBUNHABIBU <ibunhabib@gmail.com>
2020-04-15 16:34:46 +0000

    finalizing test

--- spec/features/friend_request/list_of_my_friends_spec.rb
+++ spec/features/friend_request/list_of_my_friends_spec.rb
@@ -12,26 +12,7 @@ RSpec.feature ' listing all my friends ' do
     expect(page).to have_content(@juma.name)
     expect(page).to have_content(@juma.email)
     expect(page).to have_content(@juma.friends.count)
-
-    href = "/friendships?friend_id=#{@juma.id}"
-    expect(page).not_to have_link('Add Friend', href: href)
-
-    link = "a[href='/users']"
-    find(link)
-    expect(page).to have_link('Add Friend')
-  end
-  scenario 'List of all my friends' do
-    visit root_path
     expect(page).to have_link('Friends')
-    expect(page).to have_content(@juma.name)
-    expect(page).to have_content(@juma.email)
-    expect(page).to have_content(@juma.friends.count)
-
-    href = "/friendships?friend_id=#{@juma.id}"
-    expect(page).not_to have_link('Add Friend', href: href)
-
-    link = "a[href='/users']"
-    find(link)
-    expect(page).to have_link('Add Friend')
   end
+  
 end
